56ff4d7e001895589fb656ef0b53ae826e29b663
vipulpathak113 <vipulpathak113@gmail.com>
2023-06-02 01:30:00 +0530

    navbar logo added

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,6 +1,20 @@
 import Link from "next/link";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+import Image from "next/image";
 
 export default function Nav() {
-  return <div>Nav</div>;
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="Promptopia Logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Promptopia</p>
+      </Link>
+    </nav>
+  );
 }
